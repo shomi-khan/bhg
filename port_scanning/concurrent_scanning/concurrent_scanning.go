13751ed37ca8228baeb9f5b2d3fcfdb1f81abc78
Shomi Khan <nkskl6@gmail.com>
2026-06-25 16:10:21 +0600

    finish concurrent port scanning

--- port_scanning/concurrent_scanning/concurrent_scanning.go
+++ port_scanning/concurrent_scanning/concurrent_scanning.go
@@ -6,14 +6,24 @@ import (
 	"sync"
 )
 
-func checkPort(port int) {
+func checkOpenPort(port int) bool {
 	address := fmt.Sprintf("localhost:%d", port)
 	conn, err := net.Dial("tcp", address)
-	if err != nil {
-		return
+	if err == nil {
+		conn.Close()
+		return true
 	}
-	conn.Close()
-	fmt.Printf("Port %d is open\n", port)
+	return false
+}
+
+func worker(pools chan int, wg *sync.WaitGroup) {
+	for i := range pools {
+		isOpen := checkOpenPort(i)
+		if isOpen {
+			fmt.Printf("Port %d is open\n", i)
+		}
+		wg.Done()
+	}
 }
 
 func ScanPorts() {
@@ -21,11 +31,13 @@ func ScanPorts() {
 	var wg sync.WaitGroup
 	defer wg.Wait()
 
+	pools := make(chan int, 100)
+	for i := 0; i < cap(pools); i++ {
+		go worker(pools, &wg)
+	}
+
 	for i := 1; i <= 65535; i++ {
 		wg.Add(1)
-		go func(port int, wg *sync.WaitGroup) {
-			defer wg.Done()
-			checkPort(i)
-		}(i, &wg)
+		pools <- i
 	}
 }
